3bfece93f3f789b5a09f3b0a5d74e8bf3e8ac689
Ricky Hougland <houglande@gmail.com>
2015-10-27 14:14:39 -0700

    add html files from project from yesterday

--- my-site.rb
+++ my-site.rb
@@ -3,6 +3,19 @@ require "sinatra"
 class MySite < Sinatra::Base
 
   get "/" do
-    "Hello World"
+    erb :index
   end
+
+  get "/about" do
+    erb :about
+  end
+
+  get "/blog" do
+    erb :about
+  end
+
+  get "/projects" do
+    erb :about
+  end
+
 end
