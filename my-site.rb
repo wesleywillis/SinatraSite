43da6d6672da5350b449c344cd7a41cde3621a9a
Wesley Willis <haileyw@gmail.com>
2015-10-27 14:33:34 -0700

    add public image files, complete wave 1

--- my-site.rb
+++ my-site.rb
@@ -1,8 +1,20 @@
 require "sinatra"
 
 class MySite < Sinatra::Base
-
-  get "/" do
-
+  get "/about.html" do
+    erb :about
   end
+
+  get "/index.html" do
+    erb :index
+  end
+
+  get "/blog.html" do
+    erb :blog
+  end
+
+  get "/project.html" do
+    erb :projects
+  end
+
 end
